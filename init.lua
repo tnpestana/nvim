23f7a420e547f5392a8229ea64b198cc3e73a750
Tiago Pestana <tnevespestana@gmail.com>
2025-09-07 10:59:22 +0100

    settings module

--- init.lua
+++ init.lua
@@ -1,16 +1,3 @@
 require("core.keymaps")
 require("core.plugins")
-
--- Copy text to system clipboard (enable copy/paste outside of nvim)
-vim.api.nvim_set_option_value("clipboard", "unnamed", {})
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
-})
+require("core.settings")
---
+++ lua/core/settings.lua
@@ -0,0 +1,14 @@
+
+-- Copy text to system clipboard (enable copy/paste outside of nvim)
+vim.api.nvim_set_option_value("clipboard", "unnamed", {})
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
