6b08bac567a12d11397c339923f82160a6c9b2ee
Tiago Pestana <tnevespestana@gmail.com>
2025-10-29 11:24:29 +0000

    update configs

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -18,10 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- import your plugins
     { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- automatically check for plugin updates
   checker = { enabled = true },
 })
